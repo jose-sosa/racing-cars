852f653bfb8dd11b4fdc749c628ff549f25d2925
Jose Sosa <josesosa@Joses-Air.home>
2017-11-21 15:01:39 -0500

    adding more interaction

--- js/script.js
+++ js/script.js
@@ -34,46 +34,69 @@ var inc = 10;
 
 submitInfo.addEventListener("click",function(event){
 
-  event.preventDefault();
+    event.preventDefault();
 
-  var vel = velInput.value;
-  var acc = accInput.value;
-  var tak = takInput.value;
-  var pcol;
-  var ccol;
+    var vel = Number(velInput.value);
+    var acc = Number(accInput.value);
+    var tak = Number(takInput.value);
+    var total = vel + acc + tak;
+    console.log(vel);
+    console.log(acc);
+    console.log(tak);
+    var pcol;
+    var ccol;
 
-for (var i = 0, length = playerColor.length; i < length; i++){
-   if (playerColor[i].checked)
-   {
-     pcol = playerColor[i].value;
-     console.log(pcol);
-    break;
-   }
-}
+    for (var i = 0, length = playerColor.length; i < length; i++){
+       if (playerColor[i].checked)
+       {
+         pcol = playerColor[i].value;
+         console.log(pcol);
+        break;
+       }
+    }
 
-for (var i = 0, length = compColor.length; i < length; i++){
-   if (compColor[i].checked)
-   {
-     ccol = compColor[i].value;
-    console.log(ccol);
-    break;
-   }
-}
+    for (var i = 0, length = compColor.length; i < length; i++){
+       if (compColor[i].checked)
+       {
+         ccol = compColor[i].value;
+        console.log(ccol);
+        break;
+       }
+    }
 
-  console.log("fubar");
-  submitLayout.classList.add("hidden");
-  racePlat.classList.remove("hidden");
-  car1.classList.add(pcol);
-  console.log(car1);
-  car2.classList.add(ccol);
-  console.log(car2);
 
+    if ( total == 10 ){
+      console.log("fubar");
+      submitLayout.classList.add("hidden");
+      racePlat.classList.remove("hidden");
+      car1.classList.add(pcol);
+      console.log(car1);
+      car2.classList.add(ccol);
+      console.log(car2);
+
+
+    }else{
+      console.log(total);
+      alert("Attributes Must Equate to 10");
+      location.reload();
+    }
 
 });
+
+
+
 
 raceStart.addEventListener("click",function(event){
 
   event.preventDefault();
+
+  var vel = Number(velInput.value);
+  var acc = Number(accInput.value);
+  var tak = Number(takInput.value);
+  var total = vel + acc + tak;
+
+  var pcol;
+  var ccol;
 
   var d = new Date();
   var n = d.getSeconds();
@@ -85,11 +108,15 @@ raceStart.addEventListener("click",function(event){
 
   car1.style.bottom = ((inc + "%"));
 
-  inc = inc + 3;
+  inc = inc + (.3*vel) + (.3*acc) + (.2*tak);
+
 
   setTimeout(function() { if ( inc > 70 ){
       announceWinner.textContent = "PLAYER 1 WINS";
     } else if ( inc < 70 ) {
         announceWinner.textContent = "COMPUTER WINS";
-    }} ,3000);
+    }
+      announceWinner.classList.remove("hidden");
+    } ,3000);
+
 });
